75c36fdbe31718b52028c811cadc58d63045736e
Vishal Ghule <vishal@cloudyuga.guru>
2018-08-02 14:30:51 +0530

    Create provider.tf

---
+++ webinar1/2-kubernetes/1-Terrafrom/provider.tf
@@ -0,0 +1,11 @@
+
+variable "do_token" {}
+variable "pub_key" {}
+variable "pvt_key" {}
+variable "ssh_fingerprint" {}
+variable "region" {}
+variable "size" {}
+
+provider "digitalocean" {
+  token = "${var.do_token}"
+}
