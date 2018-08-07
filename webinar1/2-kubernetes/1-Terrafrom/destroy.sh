518d575c5de178e1013ed8f1d3ffd941be69ff67
Vishal Ghule <vishal@cloudyuga.guru>
2018-08-07 12:03:51 +0530

    Update destroy.sh

--- webinar1/2-kubernetes/1-Terrafrom/destroy.sh
+++ webinar1/2-kubernetes/1-Terrafrom/destroy.sh
@@ -3,6 +3,6 @@
 
 terraform destroy --force -var "pub_key=~/.ssh/id_rsa.pub" -var "pvt_key=~/.ssh/id_rsa"  -var "region=ams3" -var "ssh_fingerprint=$FINGERPRINT" -var "do_token=$TOKEN" -var "size=2gb"
 rm *.txt
-rm -r terraform.tfstate.d
+rm -r terraform.tfstate*
 
 exit 0
